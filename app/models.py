b7dc1ecda7466fdd2e7a5b76b1382baa389b37fd
DavidLynch26 <lynchda78@gmail.com>
2024-03-01 00:33:06 -0500

    fixed first name

--- app/models.py
+++ app/models.py
@@ -14,8 +14,8 @@ class UserProfile(db.Model):
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(128), unique=True)
 
-    def __init__(self, first_name, last_name, user_name, password):
-        self.first_name = last_name
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.password = generate_password_hash(password)
